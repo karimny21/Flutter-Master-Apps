420803060fdfe27a74b916471931a76d6ffac8c2
karimny21 <rominda.kareem@gmail.com>
2021-09-13 18:15:23 +0700

    Today 19.9.64

---
+++ lib/body/showmornitor.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ShowMornitor extends StatefulWidget {
+  // const ShowMornitor({ Key? key }) : super(key: key);
+
+  @override
+  _ShowMornitorState createState() => _ShowMornitorState();
+}
+
+class _ShowMornitorState extends State<ShowMornitor> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Show Mornitor'),
+    );
+  }
+}
--- lib/state/authen.dart
+++ lib/state/authen.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:metouch/body/showmornitor.dart';
+import 'package:metouch/utility/dialog.dart';
 import 'package:metouch/utility/mystyle.dart';
 
 class Authen extends StatefulWidget {
@@ -15,13 +19,16 @@ class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
     screen = MediaQuery.of(context).size.width;
-
     return Scaffold(
-      backgroundColor: Colors.lime[100],
+      backgroundColor: Colors.tealAccent[100],
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             showlogo(),
+            SizedBox(
+              height: 10,
+            ),
             Mystyle().titleH1('My IoT Apps'),
             buildUser(),
             buildPassword(),
@@ -38,10 +45,31 @@ class _AuthenState extends State<Authen> {
       margin: EdgeInsets.only(top: 10),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(primary: Mystyle().bluelight),
-        onPressed: () {},
+        onPressed: () {
+          if ((user?.isEmpty ?? true) || (password?.isEmpty ?? true)) {
+            normalDialog(context, 'Have Space', 'Please Fill Every Blank');
+          } else {
+            print('user=$user  password= $password');
+            checkAuthen();
+          }
+        },
         child: Text('Login'),
       ),
     );
+  }
+
+  Future<Null> checkAuthen() async {
+    await Firebase.initializeApp().then((value) async {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: user, password: password)
+          .then((value) {
+        var myServiceRoute = MaterialPageRoute(
+            builder: (BuildContext context) => ShowMornitor());
+        Navigator.of(context)
+            .pushAndRemoveUntil(myServiceRoute, (route) => false);
+      }).catchError((onError) =>
+              normalDialog(context, onError.code, onError.message));
+    });
   }
 
   Container buildUser() {
